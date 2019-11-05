c13f7cb180959ee720e94db497359e69cc7349f0
Ashley Thomas <athom126@jhu.edu>
2019-11-05 17:39:58 -0500

    Add validation methods for payment form

--- src/thomas/halpert/etn/ValidatePayment.java
+++ src/thomas/halpert/etn/ValidatePayment.java
@@ -1,34 +1,74 @@
+/**
+ * File: ValidatePayment.java
+ * File Description: Contains a class that can be used to  
+ * validate request parameters from the payment form
+ */
+
 package thomas.halpert.etn;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Month;
 import java.util.*;
 import java.util.regex.Pattern;
 
+// TO DO: create methods to validate address, state, zip code, and expiration date
+
 public class ValidatePayment {
 	
-	ArrayList<String> errors = new ArrayList<String>();
+	String email;  // Stores email associated with user
+	String ccHolder; // Stores name of card holder
+	String ccType; // Stores credit card (CC) company
+	String ccNum; // Stores CC number
+	String month; // Stores CC expiration month
+	String year; // Stores CC expiration year
+	
+	ArrayList<String> paymentErrors = new ArrayList<String>(); // Contains any errors that occur during payment
 	
 	public ValidatePayment() {}
 	
-	/* 	Calls methods to validate each parameter
-	 *	Returns boolean indicate whether request parameters are valid
-	 *	Errors should be retrieved and displayed to use if invalid by calling getErrors() 
-	 */
-	public boolean validate(String email, String name) {
-		// Validate request parameters
-		this.validateEmail(email);
-		this.validateName(name);
-		
-		// If there are no errors, return true
-		if(errors.isEmpty()) {
-			return true;
-		}
-		return false; // Invalid request parameter(s)
+	/* Accessors */
+	public String getType() {
+		return this.ccType;
 	}
 	
-	// Validates email
-	public boolean validateEmail(String email) {
+	public String getNumber() {
+		return this.ccNum;
+	}
+	
+	public String getMonth() {
+		return this.month;
+	}
+	
+	public String getYear() {
+		return this.year;
+	}
+	
+	/* 	Calls methods to validate each parameter from the payment form
+	 *	Returns a boolean indicating whether the request parameters are valid
+	 *	Errors should be retrieved and displayed to use if invalid by calling getErrors() 
+	 */
+	public boolean validate(String email, String name, String ccType, String ccNum, String ccExpMonth, String ccExpYear) {
+		
+		// Invoke methods to validate payment request parameters
+		this.validCCType(ccType);
+		this.validCCNumber(ccNum);
+		this.validMonth(ccExpMonth);
+		this.validYear(ccExpYear);
+		this.validDate(ccExpMonth, ccExpYear);
+		
+		// If no payment errors, return true
+		if(this.paymentErrors.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	// Validates email address
+	public boolean validEmail(String email) {
+		this.email = email;
 		if(email == null || email.length() == 0) {
-			errors.add("-Email address must be entered");
+			paymentErrors.add("-Email address must be entered");
 			return false;
 		}
 		// Check if email follows proper format of an email address
@@ -36,16 +76,17 @@ public class ValidatePayment {
                       "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"; 
 		Pattern pattern = Pattern.compile(regex); // Create pattern specifying format of email address
 		if(!pattern.matcher(email).matches()) {
-			errors.add("-Invalid email address entered");
+			paymentErrors.add("-Invalid email address entered");
 		    return false;
 		}
 		return true;
 	}
 	
-	// Validates name
-	public boolean validateName(String name) {
+	// Validates card holder's name
+	public boolean validName(String name) {
+		this.ccHolder = name;
 		if(name == null || name.length() == 0)  {
-			errors.add("-Name must be entered");
+			paymentErrors.add("-Name must be entered");
 			return false;
 		}
 		// Check that name is completely alphabetical
@@ -54,11 +95,119 @@ public class ValidatePayment {
 			for(Character c : nameArray) {
 				// Ensure that name contains only letters and spaces
 				if(!Character.isLetter(c) && c != ' ') {
-					this.errors.add("-Invalid name entered");
+					this.paymentErrors.add("-Invalid name entered");
 					return false;
 				}
 			}
 		}
 		return true;
 	}
+	
+	// Validates credit card company
+	public boolean validCCType(String ccType) {
+		if(ccType == null || ccType == "") {
+			this.paymentErrors.add("A credit card type must be selected");
+			return false;
+		}
+		ArrayList<String> types = new ArrayList<String>();
+		types.add("discover");
+		types.add("mastercard");
+		types.add("visa");
+		if(!types.contains(ccType.toLowerCase())) {
+			this.paymentErrors.add("Invalid card type selected");
+			return false;
+		}
+		return true;
+	}
+	
+	// Validates credit card number
+	public boolean validCCNumber(String ccNum) {
+		if(ccNum == null || ccNum == "") {
+			this.paymentErrors.add("A credit card number must be entered");
+			return false;
+		}
+		if(ccNum.length() != 16) {
+			this.paymentErrors.add("Please enter a valid 16 digit credit card number");
+			return false;
+		}
+		for(int i = 0; i < ccNum.length(); i++) {
+			Character c = ccNum.charAt(i);
+			if(!Character.isDigit(c)) {
+				this.paymentErrors.add("Invalid credit card number entered");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Checks credit card expiration status (i.e. expired or not?)
+	public boolean validDate(String month, String year) {
+		// If the month and year are valid, check if date is in the past
+		if(validMonth(month) && validYear(year)) {
+			int mon = Month.valueOf(month.toUpperCase()).getValue(); // Get integer representation of month
+			String strDate = Integer.toString(mon)+"/"+year; 
+			try {
+				// If the date is after the current date -> Card has not expired (valid date)
+				if(new SimpleDateFormat("MM/yyyy").parse(strDate).after(new Date())) {
+					return true;
+				}
+			} catch (ParseException e) {
+				System.out.println("ParsingException occurred");
+				e.printStackTrace();
+			}
+		}
+		this.paymentErrors.add("The card entered is expired");
+		return false;
+	}
+	
+	// Validates the expiration month
+	public boolean validMonth(String month) {
+		this.month = month;
+		if(month == null || month == "") {
+			this.month = "";
+			paymentErrors.add("Expiration month must be selected");
+			return false;
+		}
+		ArrayList<String> months = new ArrayList<String>();
+		months.add("JANUARY");
+		months.add("FEBRUARY");
+		months.add("MARCH");
+		months.add("APRIL");
+		months.add("MAY");
+		months.add("JUNE");
+		months.add("JULY");
+		months.add("AUGUST");
+		months.add("SEPTEMBER");
+		months.add("OCTOBER");
+		months.add("NOVEMBER");
+		months.add("DECEMBER");
+		if(!months.contains(month.toUpperCase())) {
+			this.paymentErrors.add("Invalid expiration month entered");
+			return false;
+		}
+		return true;
+	}
+	
+	// Validates the expiration year
+	public boolean validYear(String year) {
+		this.year = year;
+		if(year == null || year == "") {
+			this.year = "";
+			this.paymentErrors.add("Expiration year must be selected");
+			return false;
+		}
+		try {
+			int yr = Integer.parseInt(year);
+			if(yr < 2019 && yr > 2030) {
+				this.paymentErrors.add("Invalid expiration year entered");
+				return false;
+			}
+		}
+		catch(NumberFormatException nfe) {
+			this.paymentErrors.add("Invalid expiration year entered");
+			return false;
+		}
+		return true;
+	}
+	
 }
